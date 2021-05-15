fa4b5807634905c51f2dcddc289d6beffb130d07
Julian Archila <julialejo2018@gmail.com>
2021-05-14 19:27:10 -0500

    Create order serializer and endpoint.

--- orders/serializers/orders.py
+++ orders/serializers/orders.py
@@ -3,12 +3,14 @@
 # Django REST Framework
 from django.db.models import fields
 from rest_framework import serializers
+from rest_framework.fields import CurrentUserDefault
 
 # Serializers
 from users.serializers import UserModelSerializer
 from products.serializers import ProductModelSerializer
 
 # Models
+from products.models import Product
 from orders.models import Order, OrderItem
 
 
@@ -26,3 +28,52 @@ class OrderModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order 
         fields = "__all__"
+
+
+class CreateOrderItemSerializer(serializers.ModelSerializer):
+    """ Create order item serializer. 
+    Serializer used to recive order imtes data in create order serializer.
+    """
+    class Meta:
+        model = OrderItem
+        fields = (
+            "product",
+            "price",
+            "quantity"
+        )
+
+
+class CreateOrderSerializer(serializers.ModelSerializer):
+    """ Create order serializer. 
+    Handles OrderItems creations and Order creation.
+    """
+    items = CreateOrderItemSerializer(many=True)
+    user = serializers.HiddenField(default=CurrentUserDefault())
+    # user = serializers.CurrentUserDefault()
+    class Meta:
+        model = Order
+        # exclude = []
+        fields = "__all__"
+
+    # def validate_items(self, data):
+    #     """ Handles order items validation and creation. """
+    #     for item in data:
+    #         try:
+    #             product = Product.objects.get(item.product)
+    #         except Product.DoesNotExist:
+    #             raise serializers.ValidationError(f"Product with id of {item.product} does not exist.")
+
+    #     return data
+
+    def create(self, data):
+        """ Handles order creation. """
+        items_data = data.pop("items")
+        order = Order.objects.create(**data)
+
+        for item in items_data:
+            OrderItem.objects.create(**item, order=order)
+
+        return order
+        
+
+        
--- orders/views/orders.py
+++ orders/views/orders.py
@@ -1,6 +1,8 @@
 """ Order views. """
 
 # Django REST Framework
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.viewsets import GenericViewSet
 from rest_framework import mixins
 
@@ -8,17 +10,32 @@ from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 
 # Serializers
-from orders.serializers import OrderModelSerializer
+from orders.serializers import OrderModelSerializer, CreateOrderSerializer
 
 # Models
 from orders.models import Order
 
-class OrderViewSet(mixins.ListModelMixin,GenericViewSet):
+class OrderViewSet(mixins.ListModelMixin,mixins.CreateModelMixin,GenericViewSet):
     """ Orders view set. """
-    serializer_class = OrderModelSerializer 
+    # serializer_class = OrderModelSerializer 
     queryset = Order.objects.all()
 
     def get_permissions(self):
         permissions = [IsAuthenticated]
         return [p() for p in permissions]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return CreateOrderSerializer
+
+        return OrderModelSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        data = OrderModelSerializer(order).data
+        return Response(data, status=status.HTTP_201_CREATED)
+
 
