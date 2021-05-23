aabe6bf15cf2d3a6a004ad74461f2389993e8c1f
Julian Archila <julialejo2018@gmail.com>
2021-05-23 17:45:02 -0500

    Remove item from cart. Permissions mising.

--- orders/views/cart.py
+++ orders/views/cart.py
@@ -1,6 +1,7 @@
 """ Cart views. """
 
 # Django REST Framework
+from orders.serializers.cart import CartItemModelSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
@@ -15,18 +16,33 @@ from orders.serializers import CartModelSerializer, AddCartItemSerializer
 
 # Models
 from orders.models import Cart
+from orders.models import CartItem
 
 
 class CartViewSet(
     mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.RetrieveModelMixin,
     mixins.CreateModelMixin,
     GenericViewSet):
     """ Cart viewset. """
     serializer_class = CartModelSerializer
 
     def get_queryset(self):
-        queryset = Cart.objects.get(user=self.request.user)
+        if self.action == "list":
+            queryset = Cart.objects.get(user=self.request.user)
+        else:
+            queryset =  CartItem.objects.all()
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            serializer_class = CartModelSerializer
+        else:
+            serializer_class = CartItemModelSerializer
+
+        return serializer_class
+
 
 
     def get_permissions(self):
@@ -40,8 +56,9 @@ class CartViewSet(
         serializer = self.get_serializer(queryset)
         return Response(serializer.data)
 
-    @action(detail=False, methods=["POST"])
-    def add(self, request, *args, **kwargs):
+
+    # @action(detail=False, methods=["POST"])
+    def create(self, request, *args, **kwargs):
         """ Add item to the cart. """
         serializer = AddCartItemSerializer(
             data=request.data,
@@ -51,6 +68,3 @@ class CartViewSet(
         cart = serializer.save()
         data = CartModelSerializer(cart).data
         return Response(data, status=status.HTTP_201_CREATED)
-    
-
-    
