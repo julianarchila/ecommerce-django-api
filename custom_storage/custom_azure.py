777e65c0ff1d9d76b92865c37c270733e9f585ce
Julian Archila <julialejo2018@gmail.com>
2021-05-25 10:53:37 -0500

    .

--- custom_storage/custom_azure.py
+++ custom_storage/custom_azure.py
@@ -1,7 +1,8 @@
 from storages.backends.azure_storage import AzureStorage
+from config.settings.base import env
 
 class PublicAzureStorage(AzureStorage):
     account_name = 'ecommercedjangostorage'
-    account_key = 'efvytQZZ9NC31h/gCRMuDGvNrMImMIyKvc3CK82aI+NDi5hhN02+do3kOopbEUDtGLrG/8v3m7ZEEDk23H+o3A=='
+    account_key = env.str("AZURE_ACCOUNT_KEY") 
     azure_container = 'static'
     expiration_secs = None
